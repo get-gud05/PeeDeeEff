a998c8ab385c471ec73102382c5cf4c68407ee40
Akshat <akshatdangwal07@gmail.com>
2025-06-20 16:48:13 +0530

    added routes for corresponding HTML pages

--- server.js
+++ server.js
@@ -9,6 +9,42 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,"templates/index.html"))
 })
 
+app.get('/extract', (req, res) => {
+  res.sendFile(path.join(__dirname, 'templates/enhancement.html'));
+});
+
+app.get('/remove', (req, res) => {
+  res.sendFile(path.join(__dirname, 'templates/enhancement.html'));
+});
+
+app.get('/reorder', (req, res) => {
+  res.sendFile(path.join(__dirname, 'templates/enhancement.html'));
+});
+
+app.get('/merge', (req, res) => {
+  res.sendFile(path.join(__dirname, 'templates/enhancement.html'));
+});
+
+app.get('/addwatermark', (req, res) => {
+  res.sendFile(path.join(__dirname, 'templates/enhancement.html'));
+});
+
+app.get('/insert', (req, res) => {
+  res.sendFile(path.join(__dirname, 'templates/enhancement.html'));
+});
+
+app.get('/create', (req, res) => {
+  res.sendFile(path.join(__dirname, 'templates/enhancement.html'));
+});
+
+app.get('/htmltopdf', (req, res) => {
+  res.sendFile(path.join(__dirname, 'templates/enhancement.html'));
+});
+
+app.get('/imagetopdf', (req, res) => {
+  res.sendFile(path.join(__dirname, 'templates/enhancement.html'));
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
 })
