9b82f8b7aaf4f8ca6af290034624bcbc2714e813
Akshat <akshatdangwal07@gmail.com>
2025-06-21 14:14:32 +0530

    final homepage changes + changed the color palette to a more professional one

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express()
 const port = 3000
 
 app.use(express.static(path.join(__dirname,'templates')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,"templates/index.html"))
