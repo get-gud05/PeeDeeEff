500f016a3219d8a8740a8bfafbc9264dfca0ff74
Akshat <akshatdangwal07@gmail.com>
2025-06-21 14:27:33 +0530

    fixed a typo and made common structure for merging page

--- server.js
+++ server.js
@@ -11,39 +11,39 @@ app.get('/', (req, res) => {
 })
 
 app.get('/extract', (req, res) => {
-  res.sendFile(path.join(__dirname, 'templates/enhancement.html'));
+  res.sendFile(path.join(__dirname, 'templates/extract.html'));
 });
 
 app.get('/remove', (req, res) => {
-  res.sendFile(path.join(__dirname, 'templates/enhancement.html'));
+  res.sendFile(path.join(__dirname, 'templates/remove.html'));
 });
 
 app.get('/reorder', (req, res) => {
-  res.sendFile(path.join(__dirname, 'templates/enhancement.html'));
+  res.sendFile(path.join(__dirname, 'templates/reorder.html'));
 });
 
 app.get('/merge', (req, res) => {
-  res.sendFile(path.join(__dirname, 'templates/enhancement.html'));
+  res.sendFile(path.join(__dirname, 'templates/merge.html'));
 });
 
 app.get('/addwatermark', (req, res) => {
-  res.sendFile(path.join(__dirname, 'templates/enhancement.html'));
+  res.sendFile(path.join(__dirname, 'templates/addwatermark.html'));
 });
 
 app.get('/insert', (req, res) => {
-  res.sendFile(path.join(__dirname, 'templates/enhancement.html'));
+  res.sendFile(path.join(__dirname, 'templates/insert.html'));
 });
 
 app.get('/create', (req, res) => {
-  res.sendFile(path.join(__dirname, 'templates/enhancement.html'));
+  res.sendFile(path.join(__dirname, 'templates/create.html'));
 });
 
 app.get('/htmltopdf', (req, res) => {
-  res.sendFile(path.join(__dirname, 'templates/enhancement.html'));
+  res.sendFile(path.join(__dirname, 'templates/htmltopdf.html'));
 });
 
 app.get('/imagetopdf', (req, res) => {
-  res.sendFile(path.join(__dirname, 'templates/enhancement.html'));
+  res.sendFile(path.join(__dirname, 'templates/imagetopdf.html'));
 });
 
 app.listen(port, () => {
